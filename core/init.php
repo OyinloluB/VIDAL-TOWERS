3578b3f1be7e8e578f7c3aaff2864f1c1aede1a7
OyinloluB <oyinloluwab@gmail.com>
2019-10-25 10:12:24 +0100

    Add

--- core/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $db->mysql_connect('localhost','root','','db_name');
-    if(mysqli_connect_errno()){
-        echo 'Database connection failed with following errors:'.mysqli_connect_error();
-        die();
-    }
-    ?>
